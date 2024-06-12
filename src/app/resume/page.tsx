2304a5555d1a29f19550566d283bf6edb320d48c
tarek.fakhr <tarek.fakhr@pharmacymarts.com>
2024-06-12 16:25:19 +0300

    feat: add education field remove desc

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -54,8 +54,6 @@ const about = {
 const experience = {
   icon: "/assets/resume/badge.svg",
   title: "My experience",
-  description:
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil autem eum possimus quia, vero officia perferendis tempore quae voluptas deserunt. Maiores dolores ipsa unde fuga sequi accusamus fugit earum aliquam?",
   items: [
     {
       company: "Pharmacy Marts",
@@ -78,8 +76,6 @@ const experience = {
 const education = {
   icon: "/assets/resume/cap.svg",
   title: "Education",
-  description:
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil autem eum possimus quia, vero officia perferendis tempore quae voluptas deserunt. Maiores dolores ipsa unde fuga sequi accusamus fugit earum aliquam?",
   items: [
     {
       institution: "Al-Azhar university",
@@ -96,8 +92,6 @@ const education = {
 //skills
 const skills = {
   title: "My Skills",
-  description:
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil autem eum possimus quia, vero officia perferendis tempore quae voluptas deserunt. Maiores dolores ipsa unde fuga sequi accusamus fugit earum aliquam?",
   skillList: [
     {
       icon: <FaHtml5 />,
@@ -166,10 +160,7 @@ const resume = () => {
           <div className="min-h-[70vh] w-full">
             <TabsContent value="experience" className="w-full">
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
-              <h3 className="text-4xl font-bold">{experience.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                  {experience.description}
-                </p>
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
                 <ScrollArea className="h-[400px]">
                   <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px] ">
                     {experience.items.map((item, index) => {
@@ -179,7 +170,9 @@ const resume = () => {
                           className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1 "
                         >
                           <span className="text-accent">{item.duration}</span>
-                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                          </h3>
                           <div className="flex items-center gap-3">
                             <span className="w-[6px] h-[6px] rounded-full bg-accent "></span>
                             <p className="text-white/60">{item.company}</p>
@@ -192,7 +185,30 @@ const resume = () => {
               </div>
             </TabsContent>
             <TabsContent value="education" className="w-full">
-              eductation
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px] ">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1 "
+                        >
+                          <span className="text-accent">{item.year}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.field}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent "></span>
+                            <p className="text-white/60">{item.institution}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
             <TabsContent value="skills" className="w-full">
               skills
