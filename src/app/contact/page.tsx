8a77e180b5e2a1f5289f0efbc29b1c1bb85bd275
tarek.fakhr <tarek.fakhr@pharmacymarts.com>
2024-06-22 17:37:44 +0300

    feat: add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
-
+"use client";
+import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { Select, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    descrtiption: "+201117405320",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    descrtiption: "tare2fakhr@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    descrtiption: "Cairo, Egypt",
+  },
+];
 const contact = () => {
   return (
-    <div>contact</div>
-  )
-}
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 1.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <form
+              action="https://formsubmit.co/tare2fakhr@gmail.com"
+              method="POST"
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+            >
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
+              <Button className="max-w-40 ml-auto">
+                <a href="mailto:tare2fakhr@gmail.com">Send message</a>
+              </Button>
+            </form>
+          </div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, i) => (
+                <li key={i} className="flex items-center gap-6">
+                  <div className=" size-[52px] xl:size-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                    <div className="text-[28px]">{item.icon}</div>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+                    <h3 className="text-xl">{item.descrtiption}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
 
-export default contact
+export default contact;
